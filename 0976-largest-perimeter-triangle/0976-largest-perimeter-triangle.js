204b7b3bba8e0fbb1c75597e74385c52c4c810cf
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-05-14 23:42:46 +0530

    Time: 86 ms (30.77%), Space: 52.5 MB (54.49%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.js
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.js
@@ -3,20 +3,13 @@
  * @return {number}
  */
 var largestPerimeter = function(nums) {
-    // nums.sort((a,b)=>b-a);
-    // let n=nums.length;
-    // for(let i=0;i<n-2;i++)
-    //     {
-    //         if(nums[i]<nums[i+1]+nums[i+2]);
-    //           return nums[i]+nums[i+1]+nums[i+2];
-    //     }
-    // return 0;
-    nums.sort((a, b) => b - a);
-    let n = nums.length;
-    for (let i = 0; i < n - 2; i++) {
-        if (nums[i] < nums[i + 1] + nums[i + 2]) { // removed the semicolon here
-            return nums[i] + nums[i + 1] + nums[i+2];
+    nums.sort((a,b)=>b-a);
+    let n=nums.length;
+    for(let i=0;i<n-2;i++)
+        {
+            if(nums[i]<nums[i+1]+nums[i+2])
+              return nums[i]+nums[i+1]+nums[i+2];
         }
-    }
     return 0;
+    
 };
