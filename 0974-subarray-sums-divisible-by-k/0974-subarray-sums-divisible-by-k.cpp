4728ba43e1a59e8b40b825e05edc964927c14b54
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-06-14 18:10:54 +0530

    Time: 32 ms (79.95%), Space: 32.4 MB (95.43%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
+    
     int subarraysDivByK(vector<int>& nums, int k) {
-     int res = 0; 
-    int prefix = 0;  
-    vector<int> map(k);  // Hash map to count occurrences of each remainder
-    map[0] = 1;  
-    for (int i = 0; i < nums.size(); i++) {
-        prefix = (prefix + nums[i] % k + k) % k;
-        res += map[prefix];
-        map[prefix]++;
+    int res=0; 
+    int pre=0;  
+    vector<int>map(k);
+    map[0]=1;  
+    for (int i=0;i<nums.size();i++)
+    {
+        pre=(pre+nums[i]%k+k)%k;
+        res+=map[pre];
+        map[pre]++;
     }
     return res;
-        
     }
 };
