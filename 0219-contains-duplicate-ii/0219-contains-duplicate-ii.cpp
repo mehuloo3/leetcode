d24d9338b8fe98dbd8059756aa36d497ca11aa9c
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-06-20 18:39:15 +0530

    Time: 1847 ms (5.11%), Space: 62.6 MB (97.94%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -3,7 +3,7 @@ public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         for(int i=0;i<nums.size();i++)
         {
-            for(int j=i+1;j <=i+k && j < nums.size();j++)
+            for(int j=i+1;j<=i+k && j<nums.size();j++)
             {
                if(nums[i]==nums[j] && abs(i-j)<=k)
           {
