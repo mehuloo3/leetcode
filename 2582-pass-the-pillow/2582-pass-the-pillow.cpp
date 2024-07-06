08af95e2704144771b8c66e7267301753c38cd6d
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-07-06 13:42:30 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (71.18%) - LeetHub

--- 2582-pass-the-pillow/2582-pass-the-pillow.cpp
+++ 2582-pass-the-pillow/2582-pass-the-pillow.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     int passThePillow(int n, int time) {
-        // int x=time/(n-1);
-        // int y=time%(n-1);
         if(time/(n-1)%2==0)
         {
             return time%(n-1)+1;
