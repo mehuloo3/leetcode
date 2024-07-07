c7830f11f75e58cf711b2530e063d0a5474aed9c
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-07-07 23:37:05 +0530

    Time: 3 ms (13.78%), Space: 7.2 MB (30.43%) - LeetHub

---
+++ 1518-water-bottles/1518-water-bottles.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        
+        int ans=numBottles;
+        while(numBottles>=numExchange)
+        {
+            // int newbottles=numBottles/numExchange;
+            // int remain=numBottles%numExchange;
+            // result=newbottles;
+            // newbottles+=remain;
+            while(numBottles>=numExchange){
+            int newBottles=numBottles/numExchange;
+            int remBottles= numBottles % numExchange;
+            ans=ans+newBottles;
+            numBottles=newBottles+remBottles;
+
+        }
+        }
+        return  ans;
+    }
+};
