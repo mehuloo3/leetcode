64b38badbbb7f555419db2c82b551b6a120441b6
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-07-12 22:45:43 +0530

    Time: 164 ms (16.67%), Space: 69.2 MB (33.33%) - LeetHub

---
+++ 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.js
@@ -0,0 +1,40 @@
+/**
+ * @param {string} s
+ * @param {number} x
+ * @param {number} y
+ * @return {number}
+ */
+
+var maximumGain = function(s, x, y) {
+    let total = 0;
+    let data = [];
+
+    if(x > y){
+        data = removeTarget(s,x,"ab")
+        s = data.join("");
+        removeTarget(s , y , "ba");
+    }
+    else{
+        data = removeTarget(s,y,"ba");
+        s = data.join("");
+        removeTarget(s , x , "ab");
+    }
+    
+       function removeTarget(str , points , pattern){
+        
+        let stack = [];
+
+        for(let char of str){
+
+            if(stack.length > 0 && stack.at(-1) + char == pattern){
+                total += points;
+                stack.pop();
+            }else{
+                stack.push(char);
+            }
+        }
+        return stack;
+    }
+
+    return total;
+};
