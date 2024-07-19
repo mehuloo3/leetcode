dbd4e610415047c2f64c1057b06756710318dd74
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-07-19 11:16:57 +0530

    Time: 20 ms (23.27%), Space: 14.2 MB (27.66%) - LeetHub

--- 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.cpp
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.cpp
@@ -2,34 +2,6 @@ class Solution {
 public:
     vector<int> luckyNumbers (vector<vector<int>>& matrix) {
      
-//         vector<int>ans;
-//         for(int i=0;i<matrix.size();i++)
-//         {
-//             int indx=0;
-//             for(int j=0;j<matrix[0].size();i++)
-//             {
-//                if(matrix[i][indx]>matrix[i][j])
-//                {
-//                    indx=j;
-//                }
-//             }
-                
-//              int x=matrix[i][indx];
-//                 bool f=1;
-//             for(int k=0;k<matrix.size();k++)
-//             {
-//                 if(matrix[k][indx]>x)
-//                 {
-//                     f=0;
-//                     break;
-//                 }
-//             }
-                
-//                 if(f==1) ans.push_back(x);
-            
-//         }
-     
-//         return ans;
         
                 vector<int> ans;
 				int n=matrix.size();
