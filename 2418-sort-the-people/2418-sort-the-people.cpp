2599cecf30a0c52c44ac5be16cd84da65ef44642
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-07-22 22:28:03 +0530

    Time: 26 ms (75.15%), Space: 26.1 MB (68.60%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.cpp
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -7,9 +7,18 @@ public:
            mp[heights[i]]=names[i];
        }
         sort(heights.begin(),heights.end());
+        reverse(heights.begin(),heights.end());
         vector<string>res(heights.size());
+        
+        // int j=0;
+        // for(int i=heights.size()-1;i>=0;i--)
+        // {
+        //     res[j]=mp[heights[i]];
+        //     j++;
+        // }
+        
         int j=0;
-        for(int i=heights.size()-1;i>=0;i--)
+        for(int i=0;i<=heights.size()-1;i++)
         {
             res[j]=mp[heights[i]];
             j++;
