fce8512b7a917132f1bdaa4af3a5df12c3a2b0b0
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-07-25 00:31:10 +0530

    Time: 10 ms (6.33%), Space: 17.6 MB (7.95%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.cpp
+++ 0257-binary-tree-paths/0257-binary-tree-paths.cpp
@@ -11,23 +11,43 @@
  */
 class Solution {
 public:
-    void attach(TreeNode* root,string x,vector<string>&res)
-    {
-        if(!root) return;
-        if(root->left==NULL && root->right==NULL)
-        {
-            x+=to_string(root->val);
-            res.push_back(x);
-        }
-        x+=to_string(root->val)+"->";
+       void newx(TreeNode* root,string temp,vector<string>&res)
+       {
+         if(root==NULL) return;
+           if(root->left==NULL && root->right==NULL)
+           {
+               temp+=to_string(root->val);
+               res.push_back(temp);
+           }
+           temp+=to_string(root->val)+"->";
+           newx(root->left,temp,res);
+           newx(root->right,temp,res);
+           
+       }
+//     void attach(TreeNode* root,string x,vector<string>&res)
+//     {
+//         if(!root) return;
+//         if(root->left==NULL && root->right==NULL)
+//         {
+//             x+=to_string(root->val);
+//             res.push_back(x);
+//         }
+        // x+=to_string(root->val)+"->";
         
-        attach(root->left,x,res);
-        attach(root->right,x,res);
-    }
+//         attach(root->left,x,res);
+//         attach(root->right,x,res);
+//     }
     vector<string> binaryTreePaths(TreeNode* root) {
+        
+        
+        
+        
+        
+        
         string x="";
         vector<string>ans;
-        attach(root,x,ans);
+        newx(root,x,ans);
+        // attach(root,x,ans);
         return ans;
     }
 };
