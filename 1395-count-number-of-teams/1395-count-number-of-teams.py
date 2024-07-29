d57f71cd09a315a03bf51b9e35970b2e29d49a0e
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-07-29 23:04:59 +0530

    Time: 717 ms (73.33%), Space: 11.6 MB (86.67%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.py
+++ 1395-count-number-of-teams/1395-count-number-of-teams.py
@@ -1,29 +1,26 @@
-class Solution {
-public:
-    int numTeams(vector<int>& rating) {
-        int total=0;
-        for(int i=0;i<rating.size();i++)
-        {
-            int a=0,b=0,c=0,d=0;
-            for(int j=i+1;j<rating.size();j++)
-            {
-             if(rating[j]<rating[i]) a++;
-             else if(rating[j]>rating[i])
-             {
-                 b++;
-             }
-                 
-            }
-            for(int j=0;j<i;j++)
-            {
-               if(rating[j]<rating[i]) c++;
-               else if(rating[j]>rating[i])
-               {
-                   d++;
-               }
-            }
-           total+=a*d+b*c; 
-        }
-        return total;
-    }
-};
+class Solution(object):
+    def numTeams(self, rating):
+        """
+        :type rating: List[int]
+        :rtype: int
+        """
+        total=0
+        n=len(rating)
+        for i in range(0,n):
+             lm=0
+             ll=0
+             rm=0
+             rl=0
+             for j in range(i+1,n):
+                if rating[j]<rating[i]:
+                    lm+=1
+                elif rating[j]>rating[i]:
+                    ll+=1
+             for j in range(i):
+                if rating[j]<rating[i]:
+                    rm+=1
+                elif rating[j]>rating[i]:
+                    rl+=1
+             total+=lm*rl+ll*rm
+        return total
+            
