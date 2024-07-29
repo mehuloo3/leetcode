3ef54fe8c42f66becb74f7cf6e58db4d00bebce5
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-07-29 23:11:48 +0530

    Time: 28 ms (78.08%), Space: 11.5 MB (63.96%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.cpp
+++ 1395-count-number-of-teams/1395-count-number-of-teams.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
     int numTeams(vector<int>& rating) {
+        int n=rating.size();
         int total=0;
-        for(int i=0;i<rating.size();i++)
+    for(int i=0;i<n;i++)
+    {
+        int lmost=0,lleast=0,rmost=0,rleast=0;
+        for(int j=i+1;j<n;j++)
         {
-            int a=0,b=0,c=0,d=0;
-            for(int j=i+1;j<rating.size();j++)
-            {
-             if(rating[j]<rating[i]) a++;
-             else if(rating[j]>rating[i])
-             {
-                 b++;
-             }
-                 
-            }
-            for(int j=0;j<i;j++)
-            {
-               if(rating[j]<rating[i]) c++;
-               else if(rating[j]>rating[i])
-               {
-                   d++;
-               }
-            }
-           total+=a*d+b*c; 
+            if(rating[j]<rating[i]) lmost++;
+            else if(rating[j]>rating[i]) lleast++;
         }
+         for(int j=0;j<i;j++)
+        {
+            if(rating[j]<rating[i]) rmost++;
+            else if(rating[j]>rating[i]) rleast++;
+        }
+        total+=lmost*rleast+lleast*rmost;
+    }
         return total;
     }
 };
