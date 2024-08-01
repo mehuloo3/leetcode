1af8c160bbe96016bd22781f3a47624806fdacca
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-08-01 23:05:52 +0530

    Time: 22 ms (75.09%), Space: 11.7 MB (37.72%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.py
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.py
@@ -1,10 +1,15 @@
 class Solution(object):
     def countSeniors(self, details):
-       res=[]
-       for i in details:
-          x=i[11:13]
-          v=int(x)
-          res.append(v)
-          p=filter(lambda x: x >60,res)
-       return len(list(p))
+       # res=[]
+       # for i in details:
+       #    x=i[11:13]
+       #    v=int(x)
+       #    res.append(v)
+       #    p=filter(lambda x: x >60,res)
+       # return len(list(p))
+         cnt=0
+         for i in details:
+            if int(i[11:13])>60:
+                cnt=cnt+1
+         return cnt
         
