76d3c6e4e4d72912dffad315e87f531999619078
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-08-07 16:30:43 +0530

    Time: 0 ms (100.00%), Space: 11.5 MB (25.91%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.cpp
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     vector<string> fizzBuzz(int n) {
-       vector<string>str;
+        vector<string>str;
         vector<string>v={"FizzBuzz","Fizz","Buzz"};
         for(int i=1;i<=n;i++)
         {
             
                 if(i%3==0 && i%5==0)
                 {
-                    // str.push_back("FizzBuzz");
                     str.push_back(v[0]);
                 }
                 else if(i%3==0)
                 {
-                    // str.push_back("Fizz");
                     str.push_back(v[1]);
                 }
                 else if(i%5==0)
                 {
-                    // str.push_back("Buzz");
                     str.push_back(v[2]);
                 }
                 else
