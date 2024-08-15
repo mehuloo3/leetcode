de2b47a5d687aa00a5e9972d18b9f842b9c379a9
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-08-15 10:36:24 +0530

    Time: 4 ms (73.05%), Space: 24.5 MB (5.44%) - LeetHub

---
+++ 3065-minimum-operations-to-exceed-threshold-value-i/3065-minimum-operations-to-exceed-threshold-value-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int cnt=0;
+        int i=0;
+        while(i<nums.size())
+        {
+            if(nums[i]<k)
+            {
+                cnt++;
+                // cnt=i;
+                // break;
+            }
+            i++;
+        }
+        return cnt;
+    
+    }
+};
