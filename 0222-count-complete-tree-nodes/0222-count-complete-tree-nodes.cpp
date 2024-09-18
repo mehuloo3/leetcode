0db75eb70d36a62ab336346af858296608a56bd5
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-09-18 09:56:18 +0530

    Time: 25 ms (41.52%), Space: 29.4 MB (38.35%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -11,28 +11,9 @@
  */
 class Solution {
 public:    
-    void count(TreeNode *root,vector<int>&ans)
-    {
-        if(root==NULL) return;
-        
-        count(root->left,ans);
-        ans.push_back(root->val);
-        count(root->right,ans);
-    }
+
     int countNodes(TreeNode* root) {
-
-       // int count=0;
-       //  vector<int>ans;
-       //  count(root,ans);
-       //  for(int i=0;i<ans.size();i++)
-       //  {
-       //      count++;
-       //  }
-       //  return count;
-
-        
         if(root==NULL) return 0;
-        
         return 1+countNodes(root->left)+countNodes(root->right);
     }
 };
