6631c08c20a33eb613f48fec106b4c47fc2ed06d
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-09-19 12:53:30 +0530

    Time: 21 ms (37.39%), Space: 25 MB (50.67%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.cpp
+++ 0941-valid-mountain-array/0941-valid-mountain-array.cpp
@@ -1,6 +1,5 @@
 class Solution {
 public:
-    
     bool left(vector<int>& arr,int mid)
     {
         for(int i=1;i<=mid;i++)
@@ -11,34 +10,29 @@ public:
     }
     bool right(vector<int>& arr,int mid,int n)
     {
-         for(int i=mid+1;i<n;i++)
+        for(int i=mid+1;i<n;i++)
         {
             if(arr[i]>=arr[i-1]) return false;
         }
         return true;
     }
     bool validMountainArray(vector<int>& arr) {
-        
-     
-        int n=arr.size();
-        if(n<3)
+       int n=arr.size();
+        int mid=0;
+        for(int i=1;i<n-1;i++)
         {
-            return false;
-        }
-       int mid = 0;
-        for (int i = 1; i < n - 1; i++) {
-            if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) {
-                mid = i;
+            if(arr[i]>arr[i-1] && arr[i]>arr[i+1])
+            {
+                mid=i;
                 break;
             }
         }
         
-        if (mid == 0) return false;
-
-        bool leftt = left(arr, mid);
-        bool rightt = right(arr, mid, n);
-
-        return leftt && rightt;
-       
+        if(mid==0) return false;
+        
+        int a=left(arr,mid);
+        int b=right(arr,mid,n);
+        
+        return a&&b;
     }
 };
