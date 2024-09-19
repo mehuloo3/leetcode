d34f313b82e82b5d83e06e23b38e149099ee73f5
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-09-19 10:24:30 +0530

    Time: 21 ms (37.39%), Space: 25 MB (50.67%) - LeetHub

---
+++ 0941-valid-mountain-array/0941-valid-mountain-array.cpp
@@ -0,0 +1,109 @@
+class Solution {
+public:
+    bool validMountainArray(vector<int>& arr) {
+        
+        int a=0;
+        int temp=1;
+        int flag=0;
+        int n=arr.size();
+//         if(n<3)
+//         {
+//             flag=1;
+//         }
+       
+//         for(int i=1;i<n;i++)
+//         {
+//             if(arr[i]>arr[i-1])
+//             {
+//                temp++;
+//             }
+//             else if(arr[i]==arr[i-1])
+//             {
+//                 flag=1;
+//                 break;
+//             }
+//             else
+//             {
+//                 break;
+//             }
+//         }
+//         if(temp<=1||temp==n)
+//         {
+//             flag=1;
+//         }
+//         else
+//         {
+//             if(flag!=1)
+//             {
+//         for(int j=temp;j<n-1;j++)
+//         {
+//             if(arr[j]<arr[j+1])
+//             {
+//              flag=1;
+//              break;
+//             }
+//             else if(arr[j]==arr[j+1])
+//             {
+//                 flag=1;
+//                 break;
+//             }
+//         }
+//         }
+//         }
+//       if(flag)
+//       return false;
+//       else
+//       return true; 
+        
+        // int a=0;
+        // int temp=1;
+        
+        if(n<3) 
+        {
+            flag=1;
+        }
+        
+        for(int i=1;i<n;i++)
+        {
+           if(arr[i]>arr[i-1])
+           {
+               temp++;
+           }
+            else if(arr[i]==arr[i-1])
+            {
+                flag=1;
+                break;
+            }
+            else
+            {
+                break;
+            }
+        }
+        if(temp<=1 || temp==n)
+        {
+            flag=1;
+            
+        }
+        else
+        {
+            if(flag!=1)
+            {
+            for(int j=temp;j<n-1;j++)
+        {
+            if(arr[j]<arr[j+1])
+            {
+             flag=1;
+             break;
+            }
+            else if(arr[j]==arr[j+1])
+            {
+                flag=1;
+                break;
+            }
+        }
+            }
+        }
+        if(flag) return false;
+        else return true;
+    }
+};
