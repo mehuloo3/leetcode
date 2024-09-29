8b97a9b8cb8179f5bb3488314d974491c1586607
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-09-29 22:37:44 +0530

    Time: 3 ms (86.97%), Space: 16.7 MB (17.31%) - LeetHub

---
+++ 1403-minimum-subsequence-in-non-increasing-order/1403-minimum-subsequence-in-non-increasing-order.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> minSubsequence(vector<int>& nums) {
+        sort(nums.rbegin(),nums.rend());
+        vector<int>ans;
+        int sum=0;
+        for(auto i:nums)
+        {
+            sum+=i;
+        }
+        int s=0;
+        for(auto i:nums)
+        {
+            s+=i;
+            ans.push_back(i);
+            if(s>sum-s)
+            {
+                break;
+            }
+        }
+        
+        return ans;
+    }
+};
