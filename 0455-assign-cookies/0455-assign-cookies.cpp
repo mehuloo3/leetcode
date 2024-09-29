bcc69dfa1d18b9eb6daa0bc4f616a117a1b83274
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-09-29 21:45:25 +0530

    Time: 45 ms (26.93%), Space: 44.8 MB (39.97%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,25 +1,40 @@
 class Solution {
 public:
-    int findContentChildren(vector<int>& g, vector<int>& s) {
-        
-        sort(g.begin(),g.end());
-        sort(s.begin(),s.end());
-        int cnt=0;
-        int i=0;
-        int j=0;
-        while(i<g.size() and j<s.size())
+void sorted(vector<int>&arr)
+    {
+        for(int i=0;i<arr.size();i++)
+        {
+        int key=arr[i];
+            int j=i-1;
+            
+            while(j>=0 and key<=arr[j])
             {
-                if(g[i]<=s[j])
-                {
-                    cnt++;
-                    j++;
-                    i++;
-                }
-              else if(g[i]>s[j])
-              {
-                 j++; 
-              }
+                arr[j+1]=arr[j];
+                j--;
             }
-        return cnt;
+            arr[j+1]=key;
+        }
+    }
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        // sorted(g);
+        // sorted(s);
+    sort(g.begin(),g.end());
+    sort(s.begin(),s.end());
+        int i=0,j=0;
+        int cnt=0;
+        while(i<g.size() and j<s.size())
+        {
+            if(g[i]<=s[j])
+            {
+                cnt++;
+                i++;
+                j++;
+            }
+            else if(g[i]>s[j])
+            {
+                j++;
+            }
+        }
+      return cnt;
     }
 };
