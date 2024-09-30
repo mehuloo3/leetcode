2ed2d001871fe999c8b8bdd47c34bfbdef33a825
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-09-30 11:40:20 +0530

    Time: 0 ms (100.00%), Space: 10.1 MB (21.80%) - LeetHub

--- 2810-faulty-keyboard/2810-faulty-keyboard.cpp
+++ 2810-faulty-keyboard/2810-faulty-keyboard.cpp
@@ -5,14 +5,27 @@ public:
         string x="";
         for(int i=0;i<s.size();i++)
         {
-            if(s[i]=='i')
+            //when got the i reverse
+//             if(s[i]=='i')
+//             {
+//                 reverse(res.begin(),res.end());
+//             }
+//             else
+//             {
+//                 res+=s[i];
+//             }
+            
+            if(s[i]!='i')
             {
-                reverse(res.begin(),res.end());
+                // reverse(res.begin(),res.end());
+                res+=s[i];
             }
             else
             {
-                res+=s[i];
+                // res+=s[i];
+                reverse(res.begin(),res.end());
             }
+            
             
             
         }
