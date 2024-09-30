6b013ce518c2e3cd32e202d4c2a47ca773315ec4
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-09-30 17:35:42 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (11.69%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        
+        for(int i=0;i<s.size();i++)
+        {
+            if(toupper(s[i]))
+            {
+                s[i]=tolower(s[i]);
+            }
+            else
+            {
+                continue;
+            }
+        }
+        return s;
+    }
+};
