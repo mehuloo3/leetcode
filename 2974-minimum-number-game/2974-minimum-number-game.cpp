06ae8d37c441859779f232a06b87de0b97e16d97
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-10-01 10:51:39 +0530

    Time: 0 ms (100.00%), Space: 27.5 MB (50.82%) - LeetHub

---
+++ 2974-minimum-number-game/2974-minimum-number-game.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> numberGame(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        for(int i=1;i<nums.size();i=i+2)
+        {
+            swap(nums[i-1],nums[i]);
+        }
+        return nums;
+    }
+};
