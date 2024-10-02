25357fdb09d237c8d1b90df804e9d6663867efd5
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-10-02 15:21:48 +0530

    Time: 21 ms (8.81%), Space: 24.7 MB (89.44%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -3,33 +3,58 @@ public:
     int trap(vector<int>& height) {
         int n=height.size();
         if(n==0) return 0;
-        int maxleft=0,maxright=0;
-        int maxheight=height[0],index=0,water=0;
-        for(int i=1;i<n;i++)
+        
+        // first pattern
+        // int maxleft=0,maxright=0;
+        // int maxheight=height[0],index=0,water=0;
+        // for(int i=1;i<n;i++)
+        // {
+        //     if(maxheight<height[i])
+        //     {
+        //         maxheight=height[i];
+        //         index=i;
+        //     }
+        // }
+        // //left part
+        // for(int i=0;i<index;i++)
+        // {
+        //     if(maxleft>height[i])
+        //         water+=maxleft-height[i];
+        //     else 
+        //     maxleft=height[i];
+        // }
+        //  //right
+        //  for(int i=n-1;i>index;i--)
+        //  {
+        //    if(maxright>height[i])
+        //      water+=maxright-height[i];
+        //     else 
+        //     maxright=height[i];
+        //  }
+        //  return water;
+        
+        //second pattern
+        
+        int l=0,r=height.size()-1;
+        int left=height[l];
+        int right=height[r];
+        int res=0;
+        while(l<r)
         {
-            if(maxheight<height[i])
+            if(left<right)
             {
-                maxheight=height[i];
-                index=i;
+               l++;
+               left=max(left,height[l]);
+               res+=left-height[l];
+            }
+            else
+            {
+                r--;
+                right=max(right,height[r]);
+                res+=right-height[r];
             }
         }
-        //left part
-        for(int i=0;i<index;i++)
-        {
-            if(maxleft>height[i])
-                water+=maxleft-height[i];
-            else 
-            maxleft=height[i];
-        }
-         //right
-         for(int i=n-1;i>index;i--)
-         {
-           if(maxright>height[i])
-             water+=maxright-height[i];
-            else 
-            maxright=height[i];
-         }
-         return water;
+        return res;
          
     }
 };
