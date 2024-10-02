6f8c1e2d9423e342d737c53d265bca841c9d26c1
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-10-02 09:44:16 +0530

    Time: 103 ms (26.95%), Space: 43.7 MB (47.33%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -1,29 +1,74 @@
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
-        vector<int>c;
-        c=arr;
-        sort(c.begin(),c.end());
-        int N=c.size();
-        map<int,int> ranks;
-        int rank = 1; 
- 
-    for(int i = 0; i < N; i++)
+        vector<int>Copy;
+        Copy=arr;
+        sort(Copy.begin(),Copy.end());
+        int n=Copy.size();
+        
+        map<int,int>ranks;       
+        int rank=1;
+        for(int i=0;i<n;i++)
+        {
+            int x=Copy[i];
+            if(ranks[x]==0)
+            {
+                ranks[x]=rank;
+                rank++;
+            }
+        }
+    for(int i=0;i<n;i++) 
     { 
- 
-        int element = c[i]; 
- 
-        if (ranks[element] == 0) 
-        { 
-            ranks[element] = rank; 
-            rank++; 
-        } 
-    }  
-    for(int i = 0; i < N; i++) 
-    { 
-        int element = arr[i]; 
-        arr[i] = ranks[arr[i]]; 
+        int x = arr[i]; 
+        arr[i]=ranks[arr[i]]; 
     } 
         return arr;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         vector<int>c;
+//         c=arr;
+//         sort(c.begin(),c.end());
+//         map<int,int>mp;
+//         int rank=1;
+//         for(int i=0;i<c.size();i++)
+//         {
+//             int x=c[i];
+//             if(mp[x]==rank)
+//             {
+//                 mp[x]=rank;
+//                 rank++;
+//             }
+//         }
+//            for(int i=0;i<c.size();i++)
+//         {
+//           int x=arr[i];
+//           arr[i]=mp[arr[i]];
+                
+//             }
+//         return arr;
     }
 };
