dbaf53c9f9d9167cd593a89302bd7009a5637b91
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-10-02 09:53:44 +0530

    Time: 114 ms (14.38%), Space: 43.7 MB (47.33%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -1,74 +1,74 @@
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
-        vector<int>Copy;
-        Copy=arr;
-        sort(Copy.begin(),Copy.end());
-        int n=Copy.size();
+//         vector<int>Copy;
+//         Copy=arr;
+//         sort(Copy.begin(),Copy.end());
+//         int n=Copy.size();
         
-        map<int,int>ranks;       
-        int rank=1;
-        for(int i=0;i<n;i++)
-        {
-            int x=Copy[i];
-            if(ranks[x]==0)
-            {
-                ranks[x]=rank;
-                rank++;
-            }
-        }
-    for(int i=0;i<n;i++) 
-    { 
-        int x = arr[i]; 
-        arr[i]=ranks[arr[i]]; 
-    } 
-        return arr;
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-//         vector<int>c;
-//         c=arr;
-//         sort(c.begin(),c.end());
-//         map<int,int>mp;
+//         map<int,int>ranks;       
 //         int rank=1;
-//         for(int i=0;i<c.size();i++)
+//         for(int i=0;i<n;i++)
 //         {
-//             int x=c[i];
-//             if(mp[x]==rank)
+//             int x=Copy[i];
+//             if(ranks[x]==0)
 //             {
-//                 mp[x]=rank;
+//                 ranks[x]=rank;
 //                 rank++;
 //             }
 //         }
-//            for(int i=0;i<c.size();i++)
-//         {
-//           int x=arr[i];
-//           arr[i]=mp[arr[i]];
-                
-//             }
+//     for(int i=0;i<n;i++) 
+//     { 
+//         int x = arr[i]; 
+//         arr[i]=ranks[arr[i]]; 
+//     } 
 //         return arr;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        vector<int>c;
+        c=arr;
+        sort(c.begin(),c.end());
+        map<int,int>mp;
+        int rank=1;
+        for(int i=0;i<c.size();i++)
+        {
+            int x=c[i];
+            if(mp[x]==0)
+            {
+                mp[x]=rank;
+                rank++;
+            }
+        }
+           for(int i=0;i<c.size();i++)
+        {
+          int x=arr[i];
+          arr[i]=mp[arr[i]];
+                
+            }
+        return arr;
     }
 };
