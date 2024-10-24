e0126a81e978647b17568625decac6031548e0eb
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-10-24 09:58:31 +0530

    Time: 2 ms (44.73%), Space: 7.7 MB (9.03%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,48 +1,26 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-
-        int d[n+1];
-        for(int i=0;i<=n;i++)
-         d[i]=0;
-
-        d[1]=1;
-
-        if(n>=2)
-          d[2]=2;
-
-        for(int i=3;i<=n;i++)
-        {
-            d[i]=d[i-1]+d[i-2];
-        } 
-      return d[n];
+    int find(int stair,int n,vector<int>&dp)
+    {
+        if(stair==n)
+            return 1;
         
-    //     if(n==1 || n==2)
-    //      return 1;
-    // return climbStairs(n-1)+climbStairs(n-2);
+        if(stair>n) 
+            return 0;
+        
+        if(dp[stair]!=-1) 
+            return dp[stair];
+        
+        dp[stair]=find(stair+2,n,dp)+find(stair+1,n,dp);
+        return dp[stair];
+    }
+     int climbStairs(int n)
+     {
+         
+         vector<int>dp(n+2,-1);
+         dp[n]=1,dp[n+1]=0;
+         return find(0,n,dp);
     }
   
 };
 
-
-//
-// if(n==1 || n==2)
-//      return 1;
-//     return climbstaire(n-1)+climbstaire(n-2);
-
-//
-// int t[46]={0};
-// if(n==1)
-// {
-//     t[n]=1;
-//     return 1;
-// }
-// if(n==2)
-// {
-//     t[n]=2;
-//     return 2;
-// }
-// else if(t[n]!=0)
-//   return t[n];
-//  table[n]=climbStairs[n-1]+climbStairs[n-2];
-//   return t[n];
