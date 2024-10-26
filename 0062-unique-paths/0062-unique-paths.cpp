05aee518dccd7c6a8cf8326e06f1b549d6613cd6
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-10-26 17:37:34 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (89.59%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,28 +1,46 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
+        
+        // memo DP
+        
+        int dp[m][n];
+        
+        for(int i=0;i<m;i++)
+            dp[i][0]=1;
+        for(int j=0;j<n;j++)
+            dp[0][j]=1;
+        
+        for(int i=1;i<m;i++)
+        {
+           for(int j=1;j<n;j++)
+           {
+              dp[i][j]=dp[i-1][j]+dp[i][j-1];
+           }
+        }
+        return dp[m-1][n-1];
         // DP
-        int path[100][100];
-        int i,j;
-        if(!m || !n)
-        {
-            return 0;
-        }
-        for(i=0;i<m;i++)
-        {
-            for(j=0;j<n;j++)
-            {
-                if(i==0 || j==0)
-                {
-                    path[i][j]=1;
-                }
-                else
-                {
-                    path[i][j]=path[i-1][j]+path[i][j-1];
-                }
-            }
-        }
-        return path[m-1][n-1];
+        // int path[100][100];
+        // int i,j;
+        // if(!m || !n)
+        // {
+        //     return 0;
+        // }
+        // for(i=0;i<m;i++)
+        // {
+        //     for(j=0;j<n;j++)
+        //     {
+        //         if(i==0 || j==0)
+        //         {
+        //             path[i][j]=1;
+        //         }
+        //         else
+        //         {
+        //             path[i][j]=path[i-1][j]+path[i][j-1];
+        //         }
+        //     }
+        // }
+        // return path[m-1][n-1];
         
         
         //res-TLE
