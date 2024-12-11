fd8c6816f81d5324cf5462c35f91a5b9d0611257
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-12-11 19:05:41 +0530

    Time: 0 ms (100.00%), Space: 35.3 MB (6.95%) - LeetHub

--- 3131-find-the-integer-added-to-array-i/3131-find-the-integer-added-to-array-i.cpp
+++ 3131-find-the-integer-added-to-array-i/3131-find-the-integer-added-to-array-i.cpp
@@ -3,18 +3,37 @@ public:
     int addedInteger(vector<int>& nums1, vector<int>& nums2) {
      int n=nums1.size();
      int m=nums2.size();
-     int sum1=0,sum2=0;
+//      int sum1=0,sum2=0;
     
+//         for(int i=0;i<n;i++)
+//         {
+//             sum1=sum1+nums1[i];
+//         }
+//         for(int i=0;i<m;i++)
+//         {
+//             sum2=sum2+nums2[i];
+//         }
+//         int a=sum2-sum1;
+        
+//         return a/n;
+        
+        // also use the min 
+        
+        int min1=nums1[0],min2=nums2[0];
         for(int i=0;i<n;i++)
         {
-            sum1=sum1+nums1[i];
+            if(min1<nums1[i])
+            {
+                min1=nums1[i];
+            }
         }
-        for(int i=0;i<m;i++)
+         for(int i=0;i<m;i++)
         {
-            sum2=sum2+nums2[i];
+            if(min2<nums2[i])
+            {
+                min2=nums2[i];
+            }
         }
-        int a=sum2-sum1;
-        
-        return a/n;
+        return min2-min1;
     }
 };
