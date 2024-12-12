7d101687da5ae743fd54fcc25271de428954c369
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-12-12 22:55:41 +0530

    Time: 83 ms (94.32%), Space: 93.4 MB (8.10%) - LeetHub

--- 2208-minimum-operations-to-halve-array-sum/2208-minimum-operations-to-halve-array-sum.cpp
+++ 2208-minimum-operations-to-halve-array-sum/2208-minimum-operations-to-halve-array-sum.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     int halveArray(vector<int>& nums) {
-       priority_queue<double>Q;
-       double sum=0;
-       int cnt=0;
+        priority_queue<double>Q;
+        double sum=0;
         for(auto i:nums)
         {
             sum+=i;
             Q.push(i);
         }
+        int cnt=0;
         double compare=sum;
         while(!Q.empty())
         {
-            double x=Q.top();        
+            double x=Q.top();
             Q.pop();
+            
             compare=compare-(x/2);
+            //base cond. for the termination
             if(compare<=(sum/2))
             {
                 cnt++;
@@ -25,5 +27,57 @@ public:
             
         }
         return cnt;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//        priority_queue<double>Q;
+//        double sum=0;
+//        int cnt=0;
+//         for(auto i:nums)
+//         {
+//             sum+=i;
+//             Q.push(i);
+//         }
+//         double compare=sum;
+//         while(!Q.empty())
+//         {
+//             double x=Q.top();        
+//             Q.pop();
+//             compare=compare-(x/2);
+//             if(compare<=(sum/2))
+//             {
+//                 cnt++;
+//                 break;
+//             }
+//             cnt++;
+//             Q.push(x/2);
+            
+//         }
+//         return cnt;
     }
 };
