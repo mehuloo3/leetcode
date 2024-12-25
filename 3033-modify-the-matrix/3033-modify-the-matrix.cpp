0ab6f356141142485ae575635f1b7f7aa3a8dde8
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2024-12-25 12:21:53 +0530

    Time: 3 ms (23.64%), Space: 23.6 MB (32.32%) - LeetHub

--- 3033-modify-the-matrix/3033-modify-the-matrix.cpp
+++ 3033-modify-the-matrix/3033-modify-the-matrix.cpp
@@ -2,54 +2,54 @@ class Solution {
 public:
 
 
-vector<vector<int>> modify(vector<vector<int>>& matrix) {
-    int n = matrix.size();
-    int m = matrix[0].size();
+// vector<vector<int>> modify(vector<vector<int>>& matrix) {
+//     int n = matrix.size();
+//     int m = matrix[0].size();
 
-    // Step 1: Find the maximum value for each column (ignoring -1)
-    vector<int> columnMax(m, INT_MIN);
-    for (int col = 0; col < m; col++) {
-        for (int row = 0; row < n; row++) {
-            if (matrix[row][col] != -1) {
-                columnMax[col] = max(columnMax[col], matrix[row][col]);
-            }
-        }
-    }
+//     // Step 1: Find the maximum value for each column (ignoring -1)
+//     vector<int> columnMax(m, INT_MIN);
+//     for (int col = 0; col < m; col++) {
+//         for (int row = 0; row < n; row++) {
+//             if (matrix[row][col] != -1) {
+//                 columnMax[col] = max(columnMax[col], matrix[row][col]);
+//             }
+//         }
+//     }
 
-    // Step 2: Replace -1 with the maximum value for that column
-    for (int col = 0; col < m; col++) {
-        for (int row = 0; row < n; row++) {
-            if (matrix[row][col] == -1) {
-                matrix[row][col] = columnMax[col];
-            }
-        }
-    }
+//     // Step 2: Replace -1 with the maximum value for that column
+//     for (int col = 0; col < m; col++) {
+//         for (int row = 0; row < n; row++) {
+//             if (matrix[row][col] == -1) {
+//                 matrix[row][col] = columnMax[col];
+//             }
+//         }
+//     }
 
-    return matrix;
-}
+//     return matrix;
+// }
 
     vector<vector<int>> modifiedMatrix(vector<vector<int>>& matrix) {
        
-       // int n=matrix.size();
-       // int m=matrix[0].size();
-       // vector<int>res(m,INT_MIN);
-       //  for(int i=0;i<m;i++)
-       //  {
-       //      for(int j=0;j<n;j++)
-       //      {
-       //            if(matrix[j][i]!=-1)
-       //               res[i]=max(res[j],matrix[j][i]);
-       //      }
-       //  }
-       // for(int i=0;i<m;i++)
-       //  {
-       //      for(int j=0;j<n;j++)
-       //      {
-       //            if(matrix[j][i]==-1)
-       //              matrix[j][i]=res[i];
-       //      }
-       //  }
+       int n=matrix.size();
+       int m=matrix[0].size();
+       vector<int>res(m,INT_MIN);
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                  if(matrix[j][i]!=-1)
+                     res[i]=max(res[i],matrix[j][i]);
+            }
+        }
+       for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                  if(matrix[j][i]==-1)
+                    matrix[j][i]=res[i];
+            }
+        }
         
-        return modify(matrix);
+        return matrix;
     }
 };
