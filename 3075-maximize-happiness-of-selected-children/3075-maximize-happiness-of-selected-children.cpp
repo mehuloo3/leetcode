646165be1cd598b5dcfc5b5f90ae2dbb09d9ac22
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2025-12-25 10:47:36 +0530

    Time: 209 ms (20.21%), Space: 110.2 MB (18.09%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -1,20 +1,39 @@
 class Solution {
 public:
     long long maximumHappinessSum(vector<int>& happiness, int k) {
-        long long total=0;
-        priority_queue<int>p(happiness.begin(),happiness.end());
-        int sum=0;
-        for(int i=0;i<k;i++)
+
+        //queue
+        // long long total=0;
+        // priority_queue<int>p(happiness.begin(),happiness.end());
+        // int sum=0;
+        // for(int i=0;i<k;i++)
+        // {
+        //     if(p.empty())
+        //     {
+        //         break;
+        //     }
+        //     int maxi=p.top();
+        //     p.pop();
+        //     int d=max(0,maxi-i);
+        //     total+=d;
+        // }
+        // return total;
+
+        //simple
+        long long sum=0;
+        sort(happiness.rbegin(),happiness.rend());
+        for(int i=0;i<happiness.size();i++)
         {
-            if(p.empty())
+            int v=happiness[i]-i;
+            if(v>0 && k>0)
+            {
+                sum+=v;
+                k--;
+            }else
             {
                 break;
             }
-            int maxi=p.top();
-            p.pop();
-            int d=max(0,maxi-i);
-            total+=d;
         }
-        return total;
+        return sum;
     }
 };
