f47dff7d9d97def29955a766942f66e601c36aca
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2026-03-11 17:13:33 +0530

    Time: 0 ms (100%), Space: 7.9 MB (26.22%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -1,18 +1,45 @@
 class Solution {
 public:
-    int bitwiseComplement(int n) {
-
-        int rem,mul=1,ans=0;
+string DectoBin(int n)
+    {
+        string s="";
         if(n==0)
-        return 1;
-  while(n)
-  {
-      rem=n%2;
-      rem=rem^1;
-      n/=2;
-      ans=rem*mul+ans;
-      mul=mul*2;
-  }
-  return ans;
+        {
+            return "0";
+        }
+        while(n!=0)
+        {
+          int dig=n%2;
+          s+=dig+'0';
+          n=n/2;
+        } 
+        reverse(s.begin(),s.end());
+        return s;   
+    }
+int BintoDec(string str) {
+    int res = 0;
+    int p = 1; 
+    for (int i = str.length() - 1; i >= 0; i--) {
+        if (str[i] == '1') {
+            res += p;
+        }
+        p *= 2; 
+    }
+    return res;
+    
+}
+    int bitwiseComplement(int n) {
+       string s=DectoBin(n);
+    for(int i=0;i<s.size();i++)
+    {
+        if(s[i]=='0')
+        {
+            s[i]='1';
+        }else
+        {
+            s[i]='0';
+        }
+    }
+        return BintoDec(s);
     }
 };
