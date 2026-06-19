9fd42636d06c156d2f168922987124604b48c8ae
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2026-06-19 20:07:38 +0530

    Time: 0 ms (100%), Space: 25.9 MB (6.15%) - LeetHub

---
+++ 2605-form-smallest-number-from-two-digit-arrays/2605-form-smallest-number-from-two-digit-arrays.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int minNumber(vector<int>& nums1, vector<int>& nums2) {
+        int x=0;
+        int c=0;
+        string y="";
+        string z="";
+        string k="";
+        int ans;
+        vector<int>v;
+         std::ranges::sort(nums1);
+         std::ranges::sort(nums2);
+         std::vector<int> intersection;
+    std::ranges::set_intersection(nums1, nums2, std::back_inserter(intersection));
+    if(intersection.size()>0)
+    {
+        ans=*min_element(intersection.begin(),intersection.end());
+    }
+                else
+                {
+                    int a=*min_element(nums1.begin(),nums1.end());
+                    int b=*min_element(nums2.begin(),nums2.end());
+                    y=to_string(a)+""+to_string(b);
+                    z=to_string(b)+""+to_string(a);
+                    if(stoi(y)>stoi(z))
+                    {
+                        k=z;
+                    }else
+                    {
+                        k=y;
+                    }
+                }
+        if(intersection.size()>0)
+        {
+            return ans;
+        }
+        return stoi(k);
+    }
+};
