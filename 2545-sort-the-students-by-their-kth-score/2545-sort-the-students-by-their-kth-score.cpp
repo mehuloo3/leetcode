5e49e0b44ac61155feb6dcd8a2c071d04f9fc0c0
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2026-06-24 23:02:15 +0530

    Time: 4 ms (24.22%), Space: 32 MB (94.18%) - LeetHub

---
+++ 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<vector<int>> sortTheStudents(vector<vector<int>>& score, int k) {
+        std::sort(score.begin(),score.end(),[k](const std::vector<int>&a,const std::vector<int>&b){
+            return a[k]>b[k];
+        });
+
+        return score;
+    }
+};
