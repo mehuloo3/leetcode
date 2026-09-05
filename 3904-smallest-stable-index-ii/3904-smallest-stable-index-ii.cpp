976fa6cc4be417fcaaa0731936759eccb54a8e52
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2026-09-05 14:31:08 +0530

    Time: 11 ms (57.63%), Space: 210.9 MB (15.41%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -21,20 +21,21 @@ public:
         // }
         // return -1;
 
-    vector<int> prefix(nums.size());
-    prefix[0] = nums[0];
-    for(int i = 1; i<nums.size();++i) {
-        prefix[i]=max(prefix[i - 1], nums[i]);
+    vector<int>Pre(nums.size());
+    Pre[0]=nums[0];
+    for(int i=1;i<nums.size();i++)
+    {
+        Pre[i]=max(Pre[i-1],nums[i]);
     }
-    vector<int> suffMin(n);
-    suffMin[n - 1] = nums[n - 1];
+    vector<int> Suff(n);
+    Suff[n-1]=nums[n-1];
 
-    for (int i = n - 2; i >= 0; --i) {
-        suffMin[i] =min(suffMin[i + 1], nums[i]);
+    for (int i=n-2;i>=0;i--) {
+        Suff[i]=min(Suff[i+1],nums[i]);
     }
    for(int i=0;i<n;i++)
    {
-       if(prefix[i]-suffMin[i]<=k)
+       if(Pre[i]-Suff[i]<=k)
        {
            return i;
        }
