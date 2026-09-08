5c330a89c19b02d9cc0e59d6a9f99682e1d2a1a6
Mehul Taviyad <144008449+mehuloo3@users.noreply.github.com>
2026-09-08 10:12:21 +0530

    Time: 0 ms (100%), Space: 8.5 MB (51.38%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countCommas(int n) {
+    int c=0;
+     if(n>999)
+     {
+        return n-1000+1;
+        // for(int i=1;i<=n;i++)
+        // {
+        //     if(n>=100)
+        //         c++;
+        // }
+     }
+//   else
+//      {
+//         c=0;
+//      }  
+     return 0;
+    }
+};
